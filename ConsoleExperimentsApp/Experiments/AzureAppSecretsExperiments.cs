93f94ec18d12d234adb04003d4a46c147e22456c
Eugene Murray <eugene.murray@gmail.com>
2026-02-07 23:10:50 +0000

    add more experiments

---
+++ ConsoleExperimentsApp/Experiments/AzureAppSecretsExperiments.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleExperimentsApp.Experiments
+{
+    internal class AzureAppSecretsExperiments
+    {
+    }
+}
